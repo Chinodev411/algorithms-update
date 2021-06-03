9128d79bb9971f18ea48bfeb11ccf5a13eebc6c5
Chinonso Ogbennia <75351523+Chinodev411@users.noreply.github.com>
2021-06-03 10:39:55 -0400

    Update solution.js

--- largest_number/solution.js
+++ largest_number/solution.js
@@ -1,1 +1,12 @@
+//Solution 1
 
+const largestNumber = (magnitude) => {
+  if (magnitude >= 0) {
+    return Number('9'.repeat(magnitude));
+  }
+  return Number('.' + '9'.repeat(-magnitude));
+}
+
+
+
+//Solution 2
